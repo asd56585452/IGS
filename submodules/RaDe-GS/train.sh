7e26eac7ac7bc0c08520434c5c59c23c8163d964
yjb6 <3066523455@qq.com>
2025-09-07 11:39:07 +0800

    fix

--- submodules/RaDe-GS/train.sh
+++ submodules/RaDe-GS/train.sh
@@ -1,5 +1,5 @@
 # Step 1: Train the Gaussian model using RaDe-GS
-python train.py -s YOUR_PATH/sear_steak/colmap_0/ --loader N3D --eval -r 2 --iterations 6000
+python train.py -s YOUR_PATH/sear_steak/colmap_0/ --loader N3D --eval -r 2 --iterations 6000 -m YOUR_PATH/sear_steak/colmap_0/test
 
 # Step 2: Compress the Gaussian points using LightGaussian
 python compress.py -m YOUR_PATH/sear_steak/colmap_0/test/ --iteration 5000 --prune_percent 0.45 --iterations 6000
