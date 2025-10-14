8a64c625febd6d98c11803b01480a969ad2d8dc4
蔡承翰 <a565854525658545256585452@gmail.com>
2025-10-14 13:20:54 +0800

    Update train.sh

--- submodules/RaDe-GS/train.sh
+++ submodules/RaDe-GS/train.sh
@@ -1,5 +1,5 @@
 # Step 1: Train the Gaussian model using RaDe-GS
-python train.py -s /home/tsaichenghan/IGS/dataset/161029_sports1/hdVideos/colmap_0/ --loader N3D --eval -r 2 --iterations 6000 -m /home/tsaichenghan/IGS/dataset/161029_sports1/hdVideos/colmap_0/test
+python train.py -s /home/tsaichenghan/IGS/dataset/161029_sports1/hdVideos/colmap_0/ -r 1 --iterations 6000 -m /home/tsaichenghan/IGS/dataset/161029_sports1/hdVideos/colmap_0/test
 
 # Step 2: Compress the Gaussian points using LightGaussian
 python compress.py -m /home/tsaichenghan/IGS/dataset/161029_sports1/hdVideos/colmap_0/test/ --iteration 5000 --prune_percent 0.45 --iterations 6000
